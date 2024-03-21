87ebaf2ec127788294342ef5d903088017391748
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-03-21 14:48:22 -0400

    chore: Add the listmutator/listmutator/benchmark.py file.

---
+++ listmutator/listmutator/benchmark.py
@@ -0,0 +1,6 @@
+"""Run a benchmark on a function provided by a list-based structure."""
+
+# ruff: noqa: PLR0913
+
+# TODO: Add the complete implementation of benchmarking functions
+# that support the creation of output like that shown in the README.md file
