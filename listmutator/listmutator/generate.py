4798cd058b310d9123ca9e5ce60ebfac77cfcf14
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-03-21 14:49:10 -0400

    chore: Add the listmutator/listmutator/generate.py file.

---
+++ listmutator/listmutator/generate.py
@@ -0,0 +1,4 @@
+"""Automatically generate data used for benchmarking."""
+
+# TODO: Automatically generate data that will support the execution of a
+# doubling experiment according to the strategy in the README.md file.
