3ae63a18236233bee37f117a67bdd30ddefa9882
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-03-21 14:44:21 -0400

    chore: Add starting version of the singlylinkedlist.py.

---
+++ listmutator/listmutator/singlylinkedlist.py
@@ -0,0 +1,12 @@
+"""A singly linked list implementation."""
+
+# TODO: Implement the following methods:
+# __init__ - Initialize the a ListNode.
+# __init__ - Initialize the singly linked list.
+# __len__ - Return the length of the list.
+# addfirst - Add a node with the provided item to the front of the list.
+# addlast - Add a node with the provided item to the end of the list.
+# removefirst - Remove the first node from the list and return its item.
+# removelast - Remove the last node from the list and return its item.
+# __add__ - Concatenate the other list to the end of this list and return the new list (i.e., +)
+# __iadd__ - Concatenate the other list to the end of this list and update the current list (i.e., +=).
