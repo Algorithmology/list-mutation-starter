f6cd56aaf6b521fcb6cc72defce299604c7a4d68
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-03-14 11:04:53 -0400

    coms: Add all of the comments to the listmutator/listmutator/singlylinkedlist.py file.

--- listmutator/listmutator/singlylinkedlist.py
+++ listmutator/listmutator/singlylinkedlist.py
@@ -1,8 +1,19 @@
 """A singly linked list implementation."""
 
-# TODO: Implement the following methods:
-# __init__ - Initialize the a ListNode.
-# __init__ - Initialize the singly linked list.
+# TODO: Provide a complete implementation for:
+# - ListNode
+# - SinglyLinkedList
+
+# TODO: Make sure that your implementation adheres to the
+# standards described in the project guidelines and on the
+# course website.
+
+# TODO: You may reuse source code from the course textbook's GitHub repository
+# or from the source web site provided that you cite your sources
+
+# TODO: Implement the following methods for a SinglyLinkedList:
+# __init__ - Initialize a contained ListNode.
+# __init__ - Initialize the doubly linked list.
 # __len__ - Return the length of the list.
 # addfirst - Add a node with the provided item to the front of the list.
 # addlast - Add a node with the provided item to the end of the list.
@@ -10,3 +21,10 @@
 # removelast - Remove the last node from the list and return its item.
 # __add__ - Concatenate the other list to the end of this list and return the new list (i.e., +)
 # __iadd__ - Concatenate the other list to the end of this list and update the current list (i.e., +=).
+
+# TODO: Make sure that your implementation of all the classes and methods
+# in this file contain type annotations and, when appropriate, at least
+# two assertions that confirm an invariant about the state of an object
+
+# TODO: All of the assertions that you add to the methods in these classes
+# should pass during their normal operation with an instance of the class
