7a9cfd519225a2fc7a21a971bf17eea8e399596c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-03-14 10:45:11 -0400

    coms: Add an additional comment to the listmutator/listmutator/benchmark.py file.

--- listmutator/listmutator/benchmark.py
+++ listmutator/listmutator/benchmark.py
@@ -4,3 +4,7 @@
 
 # TODO: Add the complete implementation of benchmarking functions
 # that support the creation of output like that shown in the README.md file
+
+# TODO: You may furnish your own functions that implement your own performance
+# benchmarking campaign provided that your implementation enables you to answer
+# your research questions and meets all of the project guidelines.
