acbe185508fb326614a09c828bf3a00bd3ab119b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-03-14 10:43:07 -0400

    coms: Add another TODO marker as a reminder in the listmutator/generate.py file.

--- listmutator/listmutator/generate.py
+++ listmutator/listmutator/generate.py
@@ -2,3 +2,7 @@
 
 # TODO: Automatically generate data that will support the execution of a
 # doubling experiment according to the strategy in the README.md file.
+
+# TODO: You may furnish your own functions that implement your own data
+# generation strategy provided that your implementation enables you to
+# answer your research questions and meets all of the project guidelines.
