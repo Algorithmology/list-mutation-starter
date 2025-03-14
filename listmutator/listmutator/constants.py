a8b6c41c0266c62bc4b82226269126b5ce533838
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-03-14 10:51:06 -0400

    coms: Add more TODO markers to the listmutator/listmutator/constants.py file.

--- listmutator/listmutator/constants.py
+++ listmutator/listmutator/constants.py
@@ -1,5 +1,11 @@
 """Define constants with dataclasses."""
 
 # TODO: define any numerical constants used in the listmutator module
+
 # TODO: make sure that you use the dataclass decorator to create the constants;
 # you can refer to your prior algorithm engineering projects for more details
+
+# TODO: a good rule of thumb for defining constants is that if a static analysis
+# tool like Ruff suggests that you should not use a "magic number" then that
+# should ultimately be a constant defined in this file. With that said, there
+# may be other constants that you want to define inside of this module.
